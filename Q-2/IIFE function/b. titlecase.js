f87618c152ed3fca184d4406b025b6f82a5ee6d5
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-21 19:29:36 +0530

    Delete Q-2/IIFE function directory

--- Q-2/IIFE function/b. titlecase.js
+++
@@ -1,7 +0,0 @@
-const a = "i am gonna be the king of pirates"
-const titlecase=(function(){
-    const b=a.split(' ')
-    for(i=0;i<b.length;i++)
-        {b[i]=b[i].charAt(0).toUpperCase()+b[i].substring(1).toLowerCase()}
-    console.log(b.join(' '))
-})()
