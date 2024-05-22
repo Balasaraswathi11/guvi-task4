5762013bdd41b6ac10841c3836babfc55c62097a
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 07:11:54 +0530

    Update e. Palindrome.js

--- Q-2/Arrow Function/e. Palindrome.js
+++ Q-2/Arrow Function/e. Palindrome.js
@@ -1,1 +1,11 @@
 
+const isPalindrome = str => 
+  str.split('').reverse().join('') === str;
+
+const array = ["madam", "hello", "level", "world", "radar", "noon"];
+
+const palindromes = array.filter(word => isPalindrome(word));
+
+console.log(palindromes);
+
+
