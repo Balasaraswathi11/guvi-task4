2cb91288ea541a05c5291932d8b1ee7857b1d508
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:04:51 +0530

    Update d. Primenum.js

--- Q-1/IIFE function/d. Primenum.js
+++ Q-1/IIFE function/d. Primenum.js
@@ -1,1 +1,9 @@
-
+(function(primenum) {
+    primenum = primenum.filter((number) => {
+        for (var i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) return false;
+        }
+        return true;
+    });
+    console.log(primenum);
+})([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
