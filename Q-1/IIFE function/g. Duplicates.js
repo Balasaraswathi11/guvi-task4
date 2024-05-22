cc537bbb246c12f7c5fca5d9ef95c74f32f3044b
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:39:46 +0530

    Update g. Duplicates.js

--- Q-1/IIFE function/g. Duplicates.js
+++ Q-1/IIFE function/g. Duplicates.js
@@ -1,1 +1,12 @@
 
+const result = ((array) => {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (newArray.indexOf(array[i]) === -1) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+})([1, 2, 2, 3, 4, 4, 5]);
+
+console.log(result); 
