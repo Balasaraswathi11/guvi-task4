3bfb5f51336b4850cf4d8204d29e9b8f018c3c22
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:37:49 +0530

    Update g. Duplicates.js

--- Q-1/Anonymous function/g. Duplicates.js
+++ Q-1/Anonymous function/g. Duplicates.js
@@ -1,1 +1,13 @@
 
+let removeDuplicates=function(array) {
+    let Array = [];
+    for (let i = 0; i < array.length; i++) {
+        if (Array.indexOf(array[i]) === -1) {
+            Array.push(array[i]);
+        }
+    }
+    return Array;
+}
+
+const result = removeDuplicates([1, 2, 2, 3, 4, 4, 5]);
+console.log(result); 
