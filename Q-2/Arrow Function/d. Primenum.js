63d4263e4a7ad8c74333f026813ed882b83e9c29
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:11:13 +0530

    Update d. Primenum.js

--- Q-2/Arrow Function/d. Primenum.js
+++ Q-2/Arrow Function/d. Primenum.js
@@ -1,1 +1,11 @@
-
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const prime = (primenum) => {
+    primenum = primenum.filter((number) => {
+        for (var i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) return false;
+        }
+        return true;
+    });
+    console.log(primenum);
+}
+prime(a);
