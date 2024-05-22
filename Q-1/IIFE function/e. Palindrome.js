a4bec74a848e93aa693d15ff5f32c9e64f3b1839
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 07:04:09 +0530

    Update e. Palindrome.js

--- Q-1/IIFE function/e. Palindrome.js
+++ Q-1/IIFE function/e. Palindrome.js
@@ -1,1 +1,11 @@
+const array = ["level", "hello", "radar", "world", "madam"];
+
+const palindromes = (function() {
+    return array.filter(function(word) {
+        const reversed = word.split("").reverse().join("");
+        return word === reversed;
+    });
+})();
+
+console.log(palindromes);
 
