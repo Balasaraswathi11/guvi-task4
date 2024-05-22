9940207de789a983e262255ecb9d2b4d02253486
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:45:23 +0530

    Update h. Rotatearr.js

--- Q-1/IIFE function/h. Rotatearr.js
+++ Q-1/IIFE function/h. Rotatearr.js
@@ -1,12 +1,12 @@
-const rotateArray = function(array, k) {
-    const n = array.length;
-    k = k % n;
-    for (let i = 0; i < k; i++) {
-        const temp = array.pop(); 
-        array.unshift(temp); 
-    }
-};
-
 const arr = [1, 2, 3, 4, 5];
-rotateArray(arr, 2);
+(function(array, k) {
+    const n = array.length;
+    k %= n; // Normalize k in case it's larger than the array length
+    for (let i = 0; i < k; i++) {
+        const temp = array.pop(); // Remove the last element
+        array.unshift(temp); // Add the last element to the beginning
+    }
+})(arr, 2);
+
 console.log(arr); // Output: [4, 5, 1, 2, 3]
+
