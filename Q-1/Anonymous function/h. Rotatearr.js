d262ac4196445cce397fdcf41bff743dc110a725
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:44:53 +0530

    Update h. Rotatearr.js

--- Q-1/Anonymous function/h. Rotatearr.js
+++ Q-1/Anonymous function/h. Rotatearr.js
@@ -1,1 +1,13 @@
+const rotateArray = function(array, k) {
+    const n = array.length;
+    k = k % n; 
+    for (let i = 0; i < k; i++) {
+        const temp = array.pop(); 
+        array.unshift(temp);
+    }
+};
+
+const arr = [1, 2, 3, 4, 5];
+rotateArray(arr, 2);
+console.log(arr);
 
