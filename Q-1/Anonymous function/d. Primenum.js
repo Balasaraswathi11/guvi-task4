5f552ae04cc7f3032c72579c3486bb8bdaaf3899
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 08:03:08 +0530

    Update d. Primenum.js

--- Q-1/Anonymous function/d. Primenum.js
+++ Q-1/Anonymous function/d. Primenum.js
@@ -1,1 +1,14 @@
+const prime=function(primenum) {
+    primenum = primenum.filter((number) => {
+        for (var i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) return false;
+        }
+        return true;
+    });
+    console.log(primenum);
+}
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+prime(numbers);
 
