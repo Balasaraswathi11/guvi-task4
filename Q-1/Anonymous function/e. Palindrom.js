c7d6466569ae93b3453e3da49d5593d6e6ebaa3b
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 07:01:49 +0530

    Update e. Palindrom.js

--- Q-1/Anonymous function/e. Palindrom.js
+++ Q-1/Anonymous function/e. Palindrom.js
@@ -1,1 +1,8 @@
+const words = ['level', 'hello', 'racecar', 'world', 'madam'];
+
+const palindromes = words.filter(function(item) {
+  return item === item.split('').reverse().join('');
+});
+
+console.log(palindromes); 
 
