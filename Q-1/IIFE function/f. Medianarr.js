8205af2d1784809b73b410c4d5648b8c8d1391db
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-22 09:14:11 +0530

    Update f. Medianarr.js

--- Q-1/IIFE function/f. Medianarr.js
+++ Q-1/IIFE function/f. Medianarr.js
@@ -1,1 +1,27 @@
+const median = (function(nums1, nums2) {
+    let s1 = nums1.length;
+    let s2 = nums2.length;
+    let index = s1 + s2;
+    let median;
+    if (s1 == 0) {
+        if (s2 % 2 == 1) {
+            median = nums2[Math.floor(index / 2)];
+        } else {
+            median = (nums2[Math.floor(index / 2) - 1] + nums2[Math.floor(index / 2)]) / 2;
+        }
+    } else {
+        for (let i = 0; i < index / 2 + 1; i++) {
+            nums1.push(nums2[i]);
+        }
+        nums1.sort((a, b) => a - b);
+        if (index % 2 == 1) {
+            median = nums1[Math.floor(index / 2)];
+        } else {
+            median = (nums1[Math.floor(index / 2) - 1] + nums1[Math.floor(index / 2)]) / 2;
+        }
+    }
+    return median;
+})([1, 3], [2]);
+
+console.log("Median is:", median);
 
